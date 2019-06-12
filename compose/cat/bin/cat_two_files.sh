a8e6d034434eeead38249a668223fe85d917717c
Vicente Guallpa <vincentguallpa@gmail.com>
2019-06-12 12:15:58 -0400

    bug fix for commenting in shell script

--- compose/cat/bin/cat_two_files.sh
+++ compose/cat/bin/cat_two_files.sh
@@ -1,4 +1,4 @@
-`The CONTAINER_VOLUME_LOCATION variable is taken from the Dockerfile ENV command not the .env file`
+# The CONTAINER_VOLUME_LOCATION variable is taken from the Dockerfile ENV command not the .env file
 cat ${CONTAINER_VOLUME_LOCATION}/echo_file_1.txt
 cat ${CONTAINER_VOLUME_LOCATION}/echo_file_2.txt
 cat ${CONTAINER_VOLUME_LOCATION}/my_file.txt
--- compose/echo/bin/echo_two_files.sh
+++ compose/echo/bin/echo_two_files.sh
@@ -1,3 +1,3 @@
-`The CONTAINER_VOLUME_LOCATION variable is taken from the Dockerfile ENV command not from the .env file`
+# The CONTAINER_VOLUME_LOCATION variable is taken from the Dockerfile ENV command not from the .env file
 echo "Hello World" > ${CONTAINER_VOLUME_LOCATION}/echo_file_1.txt
 echo "It's nice to be back" > ${CONTAINER_VOLUME_LOCATION}/echo_file_2.txt
