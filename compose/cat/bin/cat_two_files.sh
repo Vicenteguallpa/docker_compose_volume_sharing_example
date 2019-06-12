5bf42c0fcb09e02c6e901a4bc8c676537832e8d9
Vicente Guallpa <vincentguallpa@gmail.com>
2019-06-12 10:27:32 -0400

    complete code for tutorial

---
+++ compose/cat/bin/cat_two_files.sh
@@ -0,0 +1,3 @@
+cat ${CONTAINER_VOLUME_LOCATION}/echo_file_1.txt
+cat ${CONTAINER_VOLUME_LOCATION}/echo_file_2.txt
+cat ${CONTAINER_VOLUME_LOCATION}/my_file.txt
---
+++ compose/echo/bin/echo_two_files.sh
@@ -0,0 +1,2 @@
+echo "Hello World" > ${CONTAINER_VOLUME_LOCATION}/echo_file_1.txt
+echo "It's nice to be back" > ${CONTAINER_VOLUME_LOCATION}/echo_file_2.txt
